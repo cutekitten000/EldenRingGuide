f8f5961e333a9b3ddfa81b272faf5bd4f86b35a8
cutekitten000 <danilo.aguiar.dev@gmail.com>
2024-06-27 13:12:30 -0300

    Create Main.java

---
+++ src/application/Main.java
@@ -0,0 +1,10 @@
+package application;
+
+import eldenring.EldenRingProgressTracker;
+
+public class Main {
+    public static void main(String[] args) {
+        EldenRingProgressTracker elden = new EldenRingProgressTracker();
+        elden.showMainMenu();
+    }
+}
